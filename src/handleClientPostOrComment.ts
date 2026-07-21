1e63152150ae73484e5dd6459a32af6599b59e29
fsvreddit <97185438+fsvreddit@users.noreply.github.com>
2026-07-21 14:03:07 +0100

    fix: Update handleContentCreation to improve user status checks and logging

--- src/handleClientPostOrComment.ts
+++ src/handleClientPostOrComment.ts
@@ -237,7 +237,8 @@ export async function handleClientPostUpdate (event: PostUpdate, context: Trigge
 
 async function handleContentCreation (username: string, currentStatus: UserDetails, targetId: string, context: TriggerContext) {
     console.log(`Content Create: ℹ️ User ${username} has status ${currentStatus.userStatus}.`);
-    if (currentStatus.userStatus !== UserStatus.Banned) {
+    const isBannable = currentStatus.userStatus === UserStatus.Banned || (currentStatus.userStatus === UserStatus.Purged && currentStatus.lastStatus === UserStatus.Banned);
+    if (!isBannable) {
         return;
     }
 
@@ -253,7 +254,7 @@ async function handleContentCreation (username: string, currentStatus: UserDetai
         return;
     }
 
-    console.log(`Content Create: Status for ${username} is marked as banned`);
+    console.log(`Content Create: Status for ${username} is marked as ${currentStatus.userStatus}`);
 
     const subredditName = context.subredditName ?? await context.reddit.getCurrentSubredditName();
 
